2009948c4d41192430307bcaca40bb67e75f847f
psalk <93208031+psalk@users.noreply.github.com>
2021-11-13 07:13:02 +0530

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -644,7 +644,7 @@ async def set_password_zip(message):
 
 
 async def start_handler(event):
-    msg = "Hello This is TorToolkitX running on heroku an instance of <a href='https://github.com/KangersHub/TorToolkitX'>This Repo</a>. Try the repo for yourself and dont forget to put a STAR and fork."
+    msg = "BY @FURY_SL"
     await event.reply(msg, parse_mode="html")
 
 
